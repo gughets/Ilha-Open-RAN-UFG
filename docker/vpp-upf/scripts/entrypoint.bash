85d5b259bfd12c64fe40577a0149e956abd452b0
Murilo Silva <murilo.silva@rnp.br>
2025-09-25 22:00:07 +0000

    fix and improvement in VPP-UPF

--- docker/vpp-upf/scripts/entrypoint.bash
+++ docker/vpp-upf/scripts/entrypoint.bash
@@ -7,7 +7,7 @@ envsubst < /etc/vpp/startup.conf.tmpl > /etc/vpp/startup.conf
 envsubst < /etc/vpp/init.conf.tmpl > /etc/vpp/init.conf
 
 echo "Starting VPP..."
-vpp -c /etc/vpp/startup.conf &
+vpp -c /etc/vpp/startup.conf
 
 # Wait for VPP process
 until pgrep -x vpp_main > /dev/null; do
@@ -21,6 +21,23 @@ until vppctl show version &>/dev/null; do
     sleep 1
 done
 
-echo "✅ VPP is up and running!"
+SEP="=================================================="
 
+echo "✅ VPP is up and running!"
+echo "$SEP"
+echo "VPP Info:"
+vppctl -s /run/vpp/cli.sock show version
+echo "$SEP"
+
+echo "UPF GTPU Info:"
+vppctl -s /run/vpp/cli.sock show upf gtpu endpoint
+echo "$SEP"
+
+echo "UPF PFCP Info:"
+vppctl -s /run/vpp/cli.sock show upf specification release
+vppctl -s /run/vpp/cli.sock show upf pfcp endpoint
+vppctl -s /run/vpp/cli.sock show upf node-id
+echo "$SEP"
+
+# Mantém o container vivo
 sleep infinity
